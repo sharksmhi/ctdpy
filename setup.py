ceb262288b45716c12668e75ef2bed040b2bfbfa
Johansson Johannes <johannes.johansson@smhi.se>
2022-04-19 14:34:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt', 'r') as fh:
         requirements.append(line.strip())
 
 NAME = 'ctdpy'
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 README = open('READMEpypi.rst', 'r').read()
 
 setuptools.setup(
